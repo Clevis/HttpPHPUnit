556dce7833830706d1fc36d93588adbeff348a35
Jan Tvrdik <jan.tvrdik@gmail.com>
2011-07-20 15:50:42 +0200

    HttpPHPUnit: inicializace #structure neceka na DOMReady

--- HttpPHPUnit/assets/js/init.js
+++ HttpPHPUnit/assets/js/init.js
@@ -1,41 +1,41 @@
-$(document).ready(function () {
-
-	$.extend($.fn.disableTextSelect = function () {
-		return this.each(function () {
-			if ($.browser.mozilla) {
-				$(this).css('MozUserSelect', 'none');
-			} else if($.browser.msie) {
-				$(this).bind('selectstart', function () {
-					return false;
-				});
-			} else{
-				$(this).mousedown(function () {
-					return false;
-				});
-			}
-		});
+$.extend($.fn.disableTextSelect = function () {
+	return this.each(function () {
+		if ($.browser.mozilla) {
+			$(this).css('MozUserSelect', 'none');
+		} else if($.browser.msie) {
+			$(this).bind('selectstart', function () {
+				return false;
+			});
+		} else{
+			$(this).mousedown(function () {
+				return false;
+			});
+		}
 	});
+});
 
 
 // === Structure ===============================================================
 
-	var structure = $('#structure');
+var structure = $('#structure');
 
-	structure.find('a.node.open').parents('ul').show();
-	structure.find('> ul').show(); // potřeba při pouštění všech testů
+structure.find('a.node.open').parents('ul').show();
+structure.find('> ul').show(); // potřeba při pouštění všech testů
 
-	// Odstraní odkazy
-	structure.find('a.node').each(function (i, el) {
-		el = $(el);
-		el.closest('span').data('href', el.attr('href')).addClass('node');
-		el.replaceWith(el.text());
-	});
+// Odstraní odkazy
+structure.find('a.node').each(function (i, el) {
+	el = $(el);
+	el.closest('span').data('href', el.attr('href')).addClass('node');
+	el.replaceWith(el.text());
+});
 
-	structure.find('.node').disableTextSelect(); // zabrání označení položky při dvojkliku
-	structure.find('.node .editor').hide();
+structure.find('.node').disableTextSelect(); // zabrání označení položky při dvojkliku
+structure.find('.node .editor').hide();
 
-	structure.treeview();
+structure.treeview();
 
+
+$(document).ready(function () {
 
 // === Header ==================================================================
 
