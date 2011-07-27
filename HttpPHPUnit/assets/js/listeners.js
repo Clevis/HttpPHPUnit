d6ac0cb25b87e1bb315a6d4a56cc3cd3b963825e
PetrP <prochazkapp@gmail.com>
2011-07-28 00:14:27 +0200

    Udalosti jsou live, tedy vse funguje i behem nacitani

--- HttpPHPUnit/assets/js/listeners.js
+++ HttpPHPUnit/assets/js/listeners.js
@@ -1,4 +1,3 @@
-$(function () {
 
 $('.message-link').live('click', function () {
 	var p = $(this).parents('div').eq(0)
@@ -6,43 +5,27 @@ $('.message-link').live('click', function () {
 	$('span', this).toggle();
 	return false;
 });
-	$('#structure .node a.name').click(function (e) {
-		if (e.button == 0 && !e.shiftKey) {
-			$(this).closest('li').find('> .hitarea').trigger('click');
-			e.preventDefault();
-		}
-	});
 
-	$('#structure .node a.name').dblclick(function (e) {
-		location.href = this.href;
-	});
+$('#structure .node.file').live('click', function (e) {
+	if (e.button == 0 && e.shiftKey) {
+		var editor = $(this).find('.actions .editor');
+		location.href = editor.attr('href');
+		e.preventDefault();
+	}
+});
 
+$('.failure h3 > a, .error h3 > a').live('click', function (e) {
+	if (e.button == 0 && e.shiftKey) {
+		var editor = $(this).closest('h3').find('.editor a');
+		location.href = editor.attr('href');
+		e.preventDefault();
+	}
+});
 
-
-// === Prokliknutí shiftem do editoru ==========================================
-
-	$('#structure .node.file').click(function (e) {
-		if (e.button == 0 && e.shiftKey) {
-			var editor = $(this).find('.actions .editor');
-			location.href = editor.attr('href');
-			e.preventDefault();
-		}
-	});
-
-	$('.failure, .error').find('h3 > a').click(function (e) {
-		if (e.button == 0 && e.shiftKey) {
-			var editor = $(this).closest('h3').find('.editor a');
-			location.href = editor.attr('href');
-			e.preventDefault();
-		}
-	});
-
-	$('#summary .details > a').click(function (e) {
-		if (e.button == 0 && e.shiftKey) {
-			var editor = $(this).parent().find('.editor a');
-			location.href = editor.attr('href');
-			e.preventDefault();
-		}
-	});
-
+$('#summary .details > a').live('click', function (e) {
+	if (e.button == 0 && e.shiftKey) {
+		var editor = $(this).parent().find('.editor a');
+		location.href = editor.attr('href');
+		e.preventDefault();
+	}
 });
--- HttpPHPUnit/assets/js/structure.js
+++ HttpPHPUnit/assets/js/structure.js
@@ -5,3 +5,14 @@ $('> ul', open.parent()).show();
 open.parents('ul').show();
 structure.find('.node').disableTextSelect(); // zabrání označení položky při dvojkliku
 structure.treeview();
+
+$('#structure .node a.name').click(function (e) {
+	if (e.button == 0 && !e.shiftKey) {
+		$(this).closest('li').find('> .hitarea').trigger('click');
+		e.preventDefault();
+	}
+});
+
+$('#structure .node a.name').dblclick(function (e) {
+	location.href = this.href;
+});
