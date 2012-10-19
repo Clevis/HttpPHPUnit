90eef920a59a779a99e50553f990adb24e336e4b
PetrP <prochazkapp@gmail.com>
2012-10-20 01:55:34 +0200

    Added Runner\Application

---
+++ HttpPHPUnit/Runner/Application.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace HttpPHPUnit\Runner;
+
+use Nette\Object;
+use HttpPHPUnit\Rendering;
+use HttpPHPUnit\Config;
+use HttpPHPUnit\Events;
+
+
+/**
+ * @author Petr Prochazka
+ */
+class Application extends Object
+{
+
+	/** @var Runner */
+	private $runner;
+
+	/** @var Rendering\Renderer */
+	private $renderer;
+
+	/** @var Config\Configuration */
+	private $configuration;
+
+	/** @var Events\Events */
+	private $events;
+
+	public function __construct(Runner $runner, Rendering\Renderer $renderer, Config\Configuration $configuration, Events\Events $events)
+	{
+		$this->runner = $runner;
+		$this->renderer = $renderer;
+		$this->configuration = $configuration;
+		$this->events = $events;
+	}
+
+	/** Run HttpPHPUnit */
+	public function run()
+	{
+		$this->events->triggerListener('onStart');
+		$this->renderer->render();
+		$this->events->triggerListener('onEnd');
+	}
+
+}
