6dc1c7d211fae28e5898d1b5c9e2446bddac87f0
PetrP <prochazkapp@gmail.com>
2012-10-19 21:10:49 +0200

    Adapted tests for new Nette.

--- tests/boot.php
+++ tests/boot.php
@@ -1,24 +1,17 @@
 <?php
 
-define('LIBS_DIR', __DIR__ . '/../libs');
+require_once __DIR__ . '/../libs/Nette/loader.php';
+require_once __DIR__ . '/../libs/dump.php';
 
-require_once LIBS_DIR . '/Nette/loader.php';
-require_once LIBS_DIR . '/dump.php';
-
-use Nette\Diagnostics\Debugger as Debug;
-use Nette\Environment;
-use Nette\Loaders\RobotLoader;
-
-Debug::enable(false);
-Debug::$strictMode = true;
-
-Environment::setVariable('tempDir', __DIR__ . '/tmp');
-
-$r = new RobotLoader;
-$r->setCacheStorage(Environment::getContext()->cacheStorage);
-$r->addDirectory(LIBS_DIR);
-$r->addDirectory(__DIR__ . '/cases');
-$r->register();
+$configurator = new Nette\Config\Configurator;
+$configurator->enableDebugger();
+$configurator->setTempDirectory( __DIR__ . '/tmp');
+$configurator->createRobotLoader()
+	->addDirectory(__DIR__ . '/../libs')
+	->addDirectory(__DIR__ . '/cases')
+	->register()
+;
 
 require_once __DIR__ . '/TestCase.php';
 
+define('LIBS_DIR', __DIR__ . '/../libs');
