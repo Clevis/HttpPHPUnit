30610031db85639ca62c24ded608aaf717aa51aa
PetrP <prochazkapp@gmail.com>
2012-10-20 01:55:34 +0200

    Added Runner\Runner

--- HttpPHPUnit/Runner/Command.php
+++ HttpPHPUnit/Runner/Command.php
@@ -1,17 +1,28 @@
 <?php
 
-namespace HttpPHPUnit;
+namespace HttpPHPUnit\Runner;
 
 use PHPUnit_TextUI_Command;
+use HttpPHPUnit\Rendering;
+use HttpPHPUnit\Config;
+
 
 /**
+ * PHPUnit TestRunner for HttpPHPUnit.
+ *
  * @author Petr Prochazka
  */
 class Command extends PHPUnit_TextUI_Command
 {
+
+	/**
+	 * @param array {@see Config\Configuration::getArguments()}
+	 * @param Rendering\ResultPrinter
+	 */
 	public function run(array $argv, $printer = NULL)
 	{
 		$this->arguments['printer'] = $printer;
 		parent::run($argv, false);
 	}
+
 }
---
+++ HttpPHPUnit/Runner/Runner.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace HttpPHPUnit\Runner;
+
+use Nette\Object;
+use HttpPHPUnit\Config;
+use HttpPHPUnit\Events;
+use HttpPHPUnit\Rendering;
+
+
+/**
+ * Runs PHPUnit.
+ *
+ * @author Petr Prochazka
+ */
+class Runner extends Object
+{
+
+	/** @var Config\Configuration */
+	private $configuration;
+
+	/** @var Events\Events */
+	private $events;
+
+	public function __construct(Config\Configuration $configuration, Events\Events $events)
+	{
+		$this->configuration = $configuration;
+		$this->events = $events;
+	}
+
+	/**
+	 * @param Rendering\ResultPrinter
+	 * @see Command
+	 */
+	public function run(Rendering\ResultPrinter $printer)
+	{
+		$argumens = $this->configuration->getArguments();
+		$command = new Command;
+		$command->run($argumens, $printer);
+	}
+
+}
