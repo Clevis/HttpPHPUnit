f952de9d03aec791844c6661d082cbd3d5aeedca
PetrP <prochazkapp@gmail.com>
2012-10-20 01:55:34 +0200

    Added Config\Configurator

---
+++ HttpPHPUnit/Config/Configurator.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace HttpPHPUnit\Config;
+
+use Nette\Object;
+use HttpPHPUnit\Modules;
+use HttpPHPUnit\Events;
+use HttpPHPUnit\Config;
+use HttpPHPUnit\Rendering;
+use HttpPHPUnit\Runner;
+
+
+/**
+ * Configure HttpPHPUnit application run.
+ *
+ * <code>
+ * 	require_once __DIR__ . '/libs/HttpPHPUnit/init.php';
+ *
+ * 	$c = new HttpPHPUnit\Config\Configurator;
+ * 	$c->configuration->addArgument('--no-globals-backup --strict');
+ * 	$c->configuration->setTestDirectory(__DIR__ . '/cases');
+ * 	$c->createApplication()->run();
+ *
+ * </code>
+ *
+ * @property-read Config\Configuration $configuration
+ *
+ * @author Petr Prochazka
+ */
+class Configurator extends Object
+{
+
+	/** @var Config\Configuration */
+	private $configuration;
+
+	/** @var Events */
+	private $events = array();
+
+	public function __construct()
+	{
+		$this->configuration = $this->createConfiguration();
+		$this->events = $this->createEvents();
+	}
+
+	/** @return Config\Configuration */
+	public function getConfiguration()
+	{
+		return $this->configuration;
+	}
+
+	/** @return Runner\Application Ready to run. */
+	public function createApplication()
+	{
+		$events = $this->events;
+		$this->events = clone $events;
+		$configuration = clone $this->configuration;
+		$link = $this->createLink();
+		$link->applyConfiguration($configuration);
+		$info = $this->createInformation($configuration);
+		$runner = $this->createRunner($configuration, $events);
+		$renderer = $this->createRenderer($configuration, $events, $runner, $link);
+
+		$events->getAutowiring()
+			->addObject($configuration)
+			->addObject($link)
+			->addObject($info)
+			->addObject($renderer)
+			->addObject($runner)
+		;
+
+		return new Runner\Application($runner, $renderer, $configuration, $events);
+	}
+
+	/**
+	 * @param Config\Configuration
+	 * @param Events\Events
+	 * @return Runner\Runner
+	 */
+	protected function createRunner(Config\Configuration $configuration, Events\Events $events)
+	{
+		return new Runner\Runner($configuration, $events);
+	}
+
+	/**
+	 * @param Config\Configuration
+	 * @param Events\Events
+	 * @param Runner\Runner
+	 * @param Config\Link
+	 * @return Rendering\Renderer
+	 */
+	protected function createRenderer(Config\Configuration $configuration, Events\Events $events, Runner\Runner $runner, Config\Link $link)
+	{
+		return new Rendering\Renderer($configuration, $events, $runner, $link);
+	}
+
+	/**
+	 * @return Config\Configuration
+	 */
+	protected function createConfiguration()
+	{
+		return new Config\Configuration;
+	}
+
+	/**
+	 * @return Events\Events
+	 */
+	protected function createEvents()
+	{
+		return new Events\Events;
+	}
+
+	/**
+	 * @return Config\Link
+	 */
+	protected function createLink()
+	{
+		return new Config\Link($_GET, $_SERVER['REQUEST_URI'], $_SERVER['SCRIPT_FILENAME']);
+	}
+
+	/**
+	 * @return Config\Information
+	 */
+	protected function createInformation(Config\Configuration $configuration)
+	{
+		return new Config\Information($configuration);
+	}
+
+}
